a76bf3a164c886ce000b960e9cdc265560daebc7
verydecent <wnjhwng@gmail.com>
2019-06-14 01:35:07 -0700

    Sets up Route Components in App

--- grailed-clone/src/components/App/index.js
+++ grailed-clone/src/components/App/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navigation from '../Navigation';
 import LandingPage from '../Landing';
@@ -19,16 +16,16 @@ const App = () => (
   <Router>
     <div>
       <Navigation />
-
       <hr />
 
-      <Route exact path={ROUTES.LANDING} component={LandingPage} />
-      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+      <Route exact path = {ROUTES.LANDING} components={LandingPage} />
+      <Route path={ROUTES.SIGN_UP} component={SignInPage} />
+      <Route path={ROUTES.SIGN_IN} component={SignUpPage} />
       <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
       <Route path={ROUTES.HOME} component={HomePage} />
       <Route path={ROUTES.ACCOUNT} component={AccountPage} />
       <Route path={ROUTES.ADMIN} component={AdminPage} />
+
     </div>
   </Router>
 );
