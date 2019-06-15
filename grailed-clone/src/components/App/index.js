f2fe5f4109e94d2d02922eb790fd23ffdc42fee4
verydecent <wnjhwng@gmail.com>
2019-06-15 00:49:53 -0700

    Reintroduce all Routes in main App Component

--- grailed-clone/src/components/App/index.js
+++ grailed-clone/src/components/App/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navigation from '../Navigation';
@@ -15,39 +15,26 @@ import * as ROUTES from '../../constants/routes';
 class App extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       authUser: null
     };
   }
-
   render() {
     return (
       <Router>
         <div>
           <Navigation authUser={this.state.authUser} />
+          <hr />
+          <Route exact path ={ROUTES.LANDING} component={LandingPage} />
+          <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+          <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+          <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+          <Route path={ROUTES.HOME} component={HomePage} />
+          <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+          <Route path={ROUTES.ADMIN} component={AdminPage} />
         </div>
       </Router>
     );
   }
 }
-
-// const App = () => (
-//   <Router>
-//     <div>
-//       <Navigation />
-//       <hr />
-
-//       <Route exact path ={ROUTES.LANDING} component={LandingPage} />
-//       <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-//       <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-//       <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-//       <Route path={ROUTES.HOME} component={HomePage} />
-//       <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-//       <Route path={ROUTES.ADMIN} component={AdminPage} />
-
-//     </div>
-//   </Router>
-// );
-
 export default App;
