74a53457a0ddd53277fe4000dcaed88448870e35
verydecent <wnjhwng@gmail.com>
2019-06-15 00:46:44 -0700

    Navigation conditionally rendering depeneding on user's authentication session

--- grailed-clone/src/components/Navigation/index.js
+++ grailed-clone/src/components/Navigation/index.js
@@ -5,29 +5,38 @@ import SignOutButton from '../SignOut';
 
 import * as ROUTES from '../../constants/routes';
 
-const Navigation = () => (
+const Navigation = ({ authUser }) => (
   <div>
-    <ul>
-      <li>
-        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.LANDING}>Landing</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.HOME}>Home</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.ACCOUNT}>Account</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.ADMIN}>Admin</Link>
-      </li>
-      <li>
-        <SignOutButton />
-      </li>
-    </ul>
+    {authUser ? <NavigationAuth /> : <NavigationNonAuth />}
   </div>
+);
+
+const NavigationAuth = () => (
+  <ul>
+    <li>
+      <Link to={ROUTES.LANDING}>Landing</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.HOME}>Home</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.ACCOUNT}>Account </Link>
+    </li>
+    <li>
+      <SignOutButton />
+    </li>
+  </ul>
+);
+
+const NavigationNonAuth = () => (
+  <ul>
+    <li>
+      <Link to={ROUTES.LANDING}>Landing</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+    </li>
+  </ul>
 );
 
 export default Navigation;
