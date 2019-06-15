ac225a1867c0bb544d1a9c2d94585d9aef81d3d4
verydecent <wnjhwng@gmail.com>
2019-06-14 20:24:46 -0700

    Implement Input fields and assosciate with state

--- grailed-clone/src/components/SignUp/index.js
+++ grailed-clone/src/components/SignUp/index.js
@@ -36,9 +36,48 @@ class SignUpForm extends Component {
   }
 
   render() {
+
+    const {
+      username,
+      email,
+      passwordOne,
+      passwordTwo,
+      error
+    } = this.state;
+
     return (
       <form onSubmit={this.onSubmit}>
+        <input 
+          name="username"
+          value={username}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Full Name"
+        />
+        <input 
+          name="email"
+          value={email}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Email Address"
+        />
+        <input 
+          name="passwordOne"
+          value={passwordOne}
+          onChange={this.onChange}
+          type="password"
+          placeholder="Password"
+        />
+        <input 
+          name="passwordTwo"
+          value={passwordTwo}
+          onChange={this.onChange}
+          type="password"
+          placeholder="Confirm Password"
+        />
+        <button type="submit">Sign Up</button>
 
+        {error && <p>{error.message}</p>}
       </form>
     );
   }
