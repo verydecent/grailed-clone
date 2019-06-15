11dad25bf5e59a011069e128746bd46ede23d7d0
verydecent <wnjhwng@gmail.com>
2019-06-14 20:41:27 -0700

    Bug: switched bugs

--- grailed-clone/src/components/App/index.js
+++ grailed-clone/src/components/App/index.js
@@ -19,8 +19,8 @@ const App = () => (
       <hr />
 
       <Route exact path = {ROUTES.LANDING} components={LandingPage} />
-      <Route path={ROUTES.SIGN_UP} component={SignInPage} />
-      <Route path={ROUTES.SIGN_IN} component={SignUpPage} />
+      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
       <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
       <Route path={ROUTES.HOME} component={HomePage} />
       <Route path={ROUTES.ACCOUNT} component={AccountPage} />
