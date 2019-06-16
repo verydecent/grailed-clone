81a6de9ced4de9b6a52bb2f11a852896d7265321
verydecent <wnjhwng@gmail.com>
2019-06-16 00:16:32 -0700

    Encapsulate import/export

--- grailed-clone/src/components/Session/index.js
+++ grailed-clone/src/components/Session/index.js
@@ -1,5 +1,3 @@
-import React from 'react';
+import AuthUserContext from './context';
 
-const AuthUserContext = React.createContext(null);
-
-export default AuthUserContext;
+export { AuthUserContext };
